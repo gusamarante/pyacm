891f9a457cdc445b608da588ad833e5f3f975303
tennisnuts <oliver.nunn@yahoo.co.uk>
2026-02-10 10:52:58 +0000

    Initial Modiifcations

--- example_us.py
+++ example_us.py
@@ -14,14 +14,14 @@ ylds_d = pd.read_excel("sample_data/us_data.xlsx", index_col=0, sheet_name="dail
 ylds_d.index = pd.to_datetime(ylds_d.index)
 ylds_d = ylds_d / 100
 
-ylds_m = pd.read_excel("sample_data/us_data.xlsx", index_col=0, sheet_name="monthly")
-ylds_m.index = pd.to_datetime(ylds_m.index)
-ylds_m = ylds_m.resample("M").last()
-ylds_m = ylds_m / 100
+# ylds_m = pd.read_excel("sample_data/us_data.xlsx", index_col=0, sheet_name="monthly")
+# ylds_m.index = pd.to_datetime(ylds_m.index)
+# ylds_m = ylds_m.resample("M").last()
+# ylds_m = ylds_m / 100
 
 acm = NominalACM(
     curve=ylds_d,
-    curve_m=ylds_m,
+    # curve_m=ylds_m,
     n_factors=5,
     selected_maturities=[6, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120],
 )
